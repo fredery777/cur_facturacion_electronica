a7a21e18e2106c540615808443e333d9a043a5cd
fredery777 <fredery987654321@gmail.com>
2019-09-02 17:27:27 -0500

    Update test.php

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ include("HTTPRequester.php");
 
 if(!defined('BASE_URL_API_BILLING'))define('BASE_URL_API_BILLING', 'https://api.soenac.com/api/ubl2.1/');
 if(!defined('BILLING_DEBUG'))define('BILLING_DEBUG', true);//TEST
-if(!defined('TEST_SET_ID_DEBUG'))define('TEST_SET_ID_DEBUG', 'YOUR ID');//TEST
+if(!defined('TEST_SET_ID_DEBUG'))define('TEST_SET_ID_DEBUG', 'YOUR testSetId');//TEST
 if(!defined('TOKEN_BILLING'))define('TOKEN_BILLING', 'YOUR TOKEN');
 
  $url=(BILLING_DEBUG)?BASE_URL_API_BILLING.'invoice/'.BASE_URL_API_BILLING :URL_BASE_API_BILLING.'invoice';
